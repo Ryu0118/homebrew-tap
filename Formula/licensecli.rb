d7af546864b11185810b439362a91a0cbb598203
Ryu <87907656+Ryu0118@users.noreply.github.com>
2024-01-07 16:48:50 +0900

    Update and rename Licensecli.rb to licensecli.rb

--- Formula/licensecli.rb
+++ Formula/licensecli.rb
@@ -1,4 +1,4 @@
-class LicenseCLI < Formula
+class Licensecli < Formula
   desc "CLI tool for collecting library licenses"
   homepage "https://github.com/Ryu0118/LicenseCLI"
   url "https://github.com/Ryu0118/LicenseCLI/releases/download/0.1.0/licensecli_0.1.0.tar.gz"
