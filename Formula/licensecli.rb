08071736613e073bc8a92295bb6eb6f549aef537
Ryu0118 <ryunosuke.shibuya.3d@stu.hosei.ac.jp>
2024-01-08 21:58:09 +0900

    support universal binary

--- Formula/licensecli.rb
+++ Formula/licensecli.rb
@@ -2,7 +2,7 @@ class Licensecli < Formula
   desc "CLI tool for collecting library licenses"
   homepage "https://github.com/Ryu0118/LicenseCLI"
   url "https://github.com/Ryu0118/LicenseCLI/releases/download/0.1.0/licensecli_0.1.0.tar.gz"
-  sha256 "f39e54efad0a3212487e9196dae81928f58f2678405b9fc3ac47f10d86ec90ba"
+  sha256 "f92b70e2963e5cfbf12dc9c2a46e972cc43b837bcd5e0b6525acf52b9ec0b766"
   license "MIT"
 
   def install
