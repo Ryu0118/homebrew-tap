b3d25ff59499ed55db5b1786131de6ceaf0a6036
Ryu0118 <ryunosuke.shibuya.3d@stu.hosei.ac.jp>
2024-01-08 20:56:28 +0900

    fix

--- Formula/licensecli.rb
+++ Formula/licensecli.rb
@@ -2,7 +2,7 @@ class Licensecli < Formula
   desc "CLI tool for collecting library licenses"
   homepage "https://github.com/Ryu0118/LicenseCLI"
   url "https://github.com/Ryu0118/LicenseCLI/releases/download/0.1.0/licensecli_0.1.0.tar.gz"
-  sha256 "d798ab28eab02d605a0f0283f35fd04e0e3b2c52f7e14794a1456f94f279155a"
+  sha256 "f39e54efad0a3212487e9196dae81928f58f2678405b9fc3ac47f10d86ec90ba"
   license "MIT"
 
   def install
